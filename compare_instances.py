0dad0c01ca5637eca91649797463ff4ef42aead6
Ankit Mathur <ankitmathur@berkeley.edu>
2015-12-05 17:15:02 -0800

    pushed updates/new

--- compare_instances.py
+++ compare_instances.py
@@ -1,6 +1,6 @@
 import sys
 import traceback
-
+INSTANCES_PATH = 'instances/'
 def combine_instances():
     start, end = 1, 50
     lines = []
@@ -8,8 +8,7 @@ def combine_instances():
     while not done:
         with open(str(start) + '_' + str(end) + '.out', 'r') as sol:
             for each_line in sol:
-                lines.append(each_line + '\n')
-        lines[-1] = lines[-1][:-1]
+                lines.append(each_line)
         start += 50
         end += 50
         if end > 621:
@@ -21,48 +20,53 @@ def combine_instances():
             out.write(l)
 
 def obj_val(line, N, d, e):
-    ans1 = map(lambda x: (int(x) - 1), line)
-
+    ans1 = list(map(lambda x: (int(x) - 1), line))
     count1 = 0.0
     for i in range(N):
         for j in range(i + 1, N):
             if d[ans1[i]][ans1[j]] == 1:
                 count1 += 1
-    obj_val1 = count1 / e
+    if e:
+        obj_val1 = count1 / e
+    else:
+        obj_val1 = 0
     return obj_val1
 
-def evaluate_solutions(inst, f1, f2):
+def evaluate_solutions(f1, f2):
     s1 = 0
     s2 = 0
-    with open('comparison.out', 'w') as out:
-        with open(f1, "r") as fin1:
-            with open(f2, "r") as fin2:
-                for i in range(1,622):
-                    inst = str(i) + '.in'
-                    with open(inst, "r") as fin:
-                        N = int(fin.readline().split()[0])
-                        d, e = [[0 for j in range(N)] for i in range(N)], 0
-                        for i in xrange(N):
-                            d[i] = map(int, fin.readline().split())
-                            e += sum(d[i])
-                    s1_line = fin1.readline().split()
-                    s2_line = fin2.readline().split()
-                    s1_obj = obj_val(s1_line)
-                    s2_obj = obj_val(s2_line)
-                    if s1_obj > s2_obj:
-                        print('sol 1 better for', i, file=out)
-                        s1 += 1
-                    elif s1_obj == s2_obj:
-                        print('sols equal', i, file=out)
-                    else:
-                        print('sol 2 is better for', i, file=out)
-                        s2 += 1
+    with open(f1, "r") as fin1:
+        with open(f2, "r") as fin2:
+            for i in range(1,622):
+                inst = INSTANCES_PATH + str(i) + '.in'
+                with open(inst, "r") as fin:
+                    N = int(fin.readline().split()[0])
+                    d, e = [[0 for j in range(N)] for a in range(N)], 0
+                    for k in range(N):
+                        inst_list = fin.readline().split()
+                        d[k] = list(map(int, inst_list))
+                        e += sum(d[k])
+                s1_line = fin1.readline().split()
+                s2_line = fin2.readline().split()
+                s1_obj = obj_val(s1_line, N, d, e)
+                s2_obj = obj_val(s2_line, N, d, e)
+                if s1_obj > s2_obj:
+                    print('sol 1 better for ' + str(i))
+                    s1 += 1
+                elif s1_obj == s2_obj:
+                    print('sols are equal for ' + str(i))
+                else:
+                    print('sol 2 is better for ' + str(i))
+                    s2 += 1
+    print('*******************************')
     if s1 > s2:
         print('solution 1 better overall')
+        print('solution 1:', s1, ',solution2:', s2, ',equal:', 621-s1-s2)
     elif s1 == s2:
         print('solutions are equivalent')
     else:
         print('solution 2 better overall')
+        print('solution 1:', s1, ',solution2:', s2, ',equal:', 621-s1-s2)
 if __name__ == "__main__":
     argv = sys.argv[1:]
     if len(argv) == 0:
@@ -73,8 +77,10 @@ if __name__ == "__main__":
             print('USAGE: python3 compare_instances.py or python3 compare_instances.py [solution 1].out [solution 2].out')
     elif len(argv) == 2:
         try:
+            print(argv)
             evaluate_solutions(argv[0], argv[1])
         except Exception:
+            traceback.print_exc()
             print('USAGE: python3 compare_instances.py or python3 compare_instances.py [solution 1].out [solution 2].out')
     else:
         print('USAGE: python3 compare_instances.py or python3 compare_instances.py [solution 1].out [solution 2].out')
